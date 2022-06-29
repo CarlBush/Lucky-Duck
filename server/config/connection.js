e1d362168b91ba757c4909f76c28f7dd780656fb
Carl Bush <CarlBushs13@gmail.com>
2022-06-28 20:07:54 -0700

    front end start

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/luckyduck', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
