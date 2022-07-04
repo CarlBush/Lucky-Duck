213b07846b98865e1e8509397d6373332faa12eb
Carl Bush <CarlBushs13@gmail.com>
2022-07-04 11:20:53 -0700

    add sendFile.path to server.js for heroku test

--- server/server.js
+++ server/server.js
@@ -35,6 +35,16 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // integrate our apollo server with the express application as middleware
   server.applyMiddleware({ app });
 
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+  
+  //REDIRECT IF PAGE NOT VALID PAGE
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server is running on port ${PORT}!`);
