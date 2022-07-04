c3cd895dd24a92d9e31bf0eb3995f0770a1e5f66
mmilner29 <madisonm4514@gmail.com>
2022-07-04 12:01:46 -0700

    frontend functionality for signup in place

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+// import { useMutation } from '@apollo/client';
+// import { LOGIN_USER } from '../utils/mutations';
+
 const Login = () => (
   <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
     <Grid.Column style={{ maxWidth: 450 }}>
