aaccf4897530c5dd84f36b39c41e40c006e35d0f
Carl Bush <CarlBushs13@gmail.com>
2022-07-05 13:25:51 -0700

    very minor adjustments

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ const {ApolloServer} = require('apollo-server-express')
 const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection')
 
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const { authMiddleware } = require("./utils/auth");
 // create a new apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
-});git 
+});
 
 const mongoose = require("mongoose");
 // const db = require('./config/connection');
