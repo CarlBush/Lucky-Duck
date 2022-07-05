a511c28b3d66192efefe72ea12c2cc1221e2b9cf
mmilner29 <madisonm4514@gmail.com>
2022-07-04 22:54:35 -0700

    removed unneeded imports

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Icon, Header, Card, Comment } from 'semantic-ui-react';
+import { Header, Card, Comment } from 'semantic-ui-react';
 // import { Link } from 'react-router-dom';
 
 const Profile = () => {
