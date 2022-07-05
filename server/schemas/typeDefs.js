0cc234b579a3fedbe44538a82f922fcb74986bd5
Rodrigo <rodvalenciaa@gmail.com>
2022-07-05 02:32:53 -0700

    added mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,10 +28,24 @@ const typeDefs = gql`
     }
 
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         pins(username: String): [Pin]
         pin(_id: ID!): Pin
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addPin(description: String!): Pin
+        addComments(pinId: ID!, commentsBody: String!): Pin
+        addPet(petId: ID!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
     }
     
 `;
